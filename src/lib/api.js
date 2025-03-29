29a0472ad6704fe6c036285b7472400241b451c3
AJA Saajith <ajsajith99@gmail.com>
2025-03-29 23:56:59 +0530

    backend url fix

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BACKEND_URL = "http://localhost:5000"; // No trailing slash
+const BACKEND_URL = "https://aidf-horizone-hotel-managment-backend.onrender.com";
 
 export const api = createApi({
   reducerPath: "api",
