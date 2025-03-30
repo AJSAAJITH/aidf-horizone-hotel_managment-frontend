8206732770d8c21c0164d8784458ba92716b8bff
AJA Saajith <ajsajith99@gmail.com>
2025-03-30 23:56:27 +0530

    path update

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // real uri - https://aidf-horizone-hotel-managment-backend.onrender.com
-const BACKEND_URL = "http://localhost:5000";
+// const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "https://aidf-horizone-hotel-managment-backend.onrender.com";
 
 export const api = createApi({
   reducerPath: "api",
